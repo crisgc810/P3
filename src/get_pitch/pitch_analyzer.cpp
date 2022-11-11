ff41abed2745ad2b217d53970671718d846e6cb3
crisgc810 <114924341+crisgc810@users.noreply.github.com>
2022-11-11 12:31:41 +0100

    umaxnorm afegit

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -11,11 +11,17 @@ namespace upc {
   void PitchAnalyzer::autocorrelation(const vector<float> &x, vector<float> &r) const {
 
     for (unsigned int l = 0; l < r.size(); ++l) {
-  		r[l]=0;
-      for(unsigned int n=l; n<x.size(); n++){
-          r[l]+=x[n]*x[n-l];
+  		/// \TODO Compute the autocorrelation r[l]
+      /// \FET Autocorrelation computed
+      /// - Inicialitzem l'autocorrelació a 0
+      /// - Afegim el producte
+      /// - Dividim per la durada
+      /// *** TAXANNNN ***
+      r[l]=0;
+      for(unsigned int n=l; n< x.size(); n++){
+        r[l] += x[n]*x[n-l];
       }
-      r[l]=r[l]/x.size();
+      r[l] = r[l]/x.size();
     }
 
     if (r[0] == 0.0F) //to avoid log() and divide zero 
@@ -54,6 +60,8 @@ namespace upc {
     /// \TODO Implement a rule to decide whether the sound is voiced or not.
     /// * You can use the standard features (pot, r1norm, rmaxnorm),
     ///   or compute and use other ones.
+    if(rmaxnorm> umaxnorm) return false;
+  
     return true;
   }
 
@@ -79,13 +87,11 @@ namespace upc {
 	///    - The lag corresponding to the maximum value of the pitch.
     ///	   .
 	/// In either case, the lag should not exceed that of the minimum value of the pitch.
-
-  for(iR=r.begin()+npitch_min; iR<r.begin()+npitch_max; iR++){
-    if(iR>iRMax){
-      iRMax=iR;
+    for(iRMax = iR = r.begin() + npitch_min; iR< r.begin()+ npitch_max; iR++){ //Com que el màxim ha d'estar entre en màx i el mín pitch fem el for que recorri aquest espai
+      if(*iR>*iRMax){
+        iRMax= iR;   //Si el iR és més gran que el màxim, actualitzem el valor d'aquest màxim
+      }
     }
-  }
-
     unsigned int lag = iRMax - r.begin();
 
     float pot = 10 * log10(r[0]);
